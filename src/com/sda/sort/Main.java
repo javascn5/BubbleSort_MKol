4854cced6a0a5a3288bd6b1c2ce3a0ccf798d827
Kolachow <kolas3000@gmail.com>
2018-10-21 15:36:15 +0200

    Algorytm sortowania babelkowego

--- src/com/sda/sort/Main.java
+++ src/com/sda/sort/Main.java
@@ -3,6 +3,28 @@ package com.sda.sort;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println();
+        int[] tab = {5, 54, 4, 65, 65, 41, 35, 99, 1, -10, 54};
+
+        System.out.println("Nieposortowane:");
+        for (int i = 0; i < tab.length; i++){
+            System.out.print(tab[i] + " ");
+        }
+        System.out.println("\n");
+
+
+        for (int i = 0; i < tab.length - 1; i++) {
+            for (int j = 0; j < tab.length - 1; j++) {
+                if (tab[j] > tab[j + 1]) {
+                    int temp = tab[j];
+                    tab[j] = tab[j + 1];
+                    tab[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println("Posortowane:");
+        for (int i = 0; i < tab.length; i++){
+            System.out.print(tab[i] + " ");
+        }
     }
 }
