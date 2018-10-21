d62c4fcb94440593c5d6819684b5b26e0102e4b6
Kolachow <kolas3000@gmail.com>
2018-10-21 15:44:37 +0200

    Zabezpieczenie przed niepotrzebnym sortowaniem

--- src/com/sda/sort/Main.java
+++ src/com/sda/sort/Main.java
@@ -3,27 +3,37 @@ package com.sda.sort;
 public class Main {
 
     public static void main(String[] args) {
-        int[] tab = {5, 54, 4, 65, 65, 41, 35, 99, 1, -10, 54};
+        int[] tab = {11, 10, 14, 15, 20, 34, 3, 41, 42, 44, 54};
+        boolean sorted = true;
+        int qIteracji = 0;
 
         System.out.println("Nieposortowane:");
-        for (int i = 0; i < tab.length; i++){
+        for (int i = 0; i < tab.length; i++) {
             System.out.print(tab[i] + " ");
         }
         System.out.println("\n");
 
 
         for (int i = 0; i < tab.length - 1; i++) {
+            sorted = true;
             for (int j = 0; j < tab.length - 1; j++) {
                 if (tab[j] > tab[j + 1]) {
                     int temp = tab[j];
                     tab[j] = tab[j + 1];
                     tab[j + 1] = temp;
+                    sorted = false;
                 }
+                qIteracji++;
+            }
+            if (sorted == true) {
+                break;
             }
         }
 
+        System.out.println("Ilosc iteracji: " + qIteracji);
+
         System.out.println("Posortowane:");
-        for (int i = 0; i < tab.length; i++){
+        for (int i = 0; i < tab.length; i++) {
             System.out.print(tab[i] + " ");
         }
     }
